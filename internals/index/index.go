ff5bda6310c5b05a0627ab4b4b55cac1c7ff3d87
Themaytrix <obengraymond81@gmail.com>
2025-12-18 14:37:38 -0700

    implementing index and index entries

--- internals/index/index.go
+++ internals/index/index.go
@@ -13,8 +13,7 @@ type Index struct{
 
   Signature [4]byte
   Version uint32
-  Count uint32
-  Entries []*IndexEntry
+  Entries []IndexEntry
 }
 
 type IndexEntry struct{
@@ -32,50 +31,31 @@ CTimeSec  uint32
 	Flags     uint16
 	Path      string
 }
-
-// this function is to construct a new instance of the Entry
-func NewEntry(path string) (*IndexEntry, error){
-  // get file information
-  fileInfo, err := os.Stat(path)
-  if err != nil{
-    return nil,err
+// goon Index entry
+func NewIndex() *Index{
+  return &Index{
+    Signature: [4]byte{'D','I','R','C'},
+    Version: 2,
+    Entries: []IndexEntry{},
   }
-  // open file
-  file,err := os.Open(path)
-  if err != nil{
-    return nil,err
-  }
-  defer file.Close()
-  // read file contents
-  h := sha256.New()
-  io.Copy(h,file) // stream the filecontents for hashing. discard filebytes and retain whats necessary for hashing
+}
 
-  // hash file contents. execute file hash.
-  var oid [32]byte
-  copy(oid[:],h.Sum(nil)) // deep copy of hash for ObjectID in IndexEntry
-
-  // get low level fileinformation for IndexEntry
-  stat := fileInfo.Sys().(*syscall.Stat_t)
-  // write new entry 
-  entry := &IndexEntry{
-    CTimeSec: uint32(stat.Ctimespec.Sec),
-    CTimeNSec: uint32(stat.Ctimespec.Nsec),
-    MTimeSec: uint32(stat.Mtimespec.Sec),
-    MTimeNSec: uint32(stat.Mtimespec.Nsec),
-    Dev: uint32(stat.Dev),
-    Ino: uint32(stat.Ino),
-    Mode: uint32(stat.Mode),
-    UID: uint32(stat.Uid),
-    GID: uint32(stat.Gid),
-    FileSize: uint32(fileInfo.Size()),
-    ObjectID: oid,
-    Flags: uint16(len(filepath.Base(path))),
-    Path: path,
-  }
-
-  return entry, nil
-
+// add index entries to the index
+func (idx *Index) AddEntry(e IndexEntry){
+  idx.Entries = append(idx.Entries, e)
 }
 
 
-// write the Index entry into goon/index file
+// create a new indexentry
+func NewIndexEntry(path string)(IndexEntry, error){
+  // check if the file exists
+
+  info,err := os.Stat(path)
+
+  if err != nil{
+    return IndexEntry{},err
+  }
+
+  raw := info.Sys()
+
+}
