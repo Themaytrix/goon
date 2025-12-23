d891880a34d36e863e2dba92dab206b3163c1a93
Themaytrix <obengraymond81@gmail.com>
2025-12-22 18:43:06 -0700

    further helper functions

--- utils/util.go
+++ utils/util.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
-  "os"
-  "path/filepath"
+	"crypto/sha1"
+	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func IsGoonRepo(currDir, targetDir string) (string,bool) {
@@ -23,4 +25,74 @@ func IsGoonRepo(currDir, targetDir string) (string,bool) {
 
 	parentDir := filepath.Dir(currDir)
 	return IsGoonRepo(parentDir, targetDir)
+}
+
+func HashBlob(path string) ([32]byte, error){
+  var blob [32]byte
+
+  // read the contents in the file
+  content, err := os.ReadFile(path)
+
+  if err != nil{
+    return blob , err
+  }
+
+// define the header
+
+  header := fmt.Sprintf("blob %d\x00", len(content))
+
+  hash := sha1.New()
+  hash.Write([]byte(header))
+  hash.Write(content)
+
+  copy(blob[:], hash.Sum(nil))
+
+  return blob, nil
+}
+
+
+func Extractfiles(){
+
+}
+
+func IsFile(path string) bool {
+
+  info, err := os.Stat(path)
+  if err != nil{
+    return false
+  }
+   // check if regular file
+
+  return info.Mode().IsRegular()
+}
+
+func IsDir(path string) bool {
+  info, err := os.Stat(path)
+  if err != nil{
+    return false
+  }
+
+  return info.IsDir()
+}
+
+func WalkDir(files *[]string, root string) error {
+  // traverse root
+
+  return filepath.WalkDir(root,func(path string, d os.DirEntry, err error) error {
+    if err != nil{
+      return err
+    }
+
+    // check if it's directory and not .goon or .git
+    if d.IsDir() && d.Name() == ".git" && d.Name() == ".goon"{
+      return filepath.SkipDir
+    }
+
+    if !d.IsDir(){
+      *files = append(*files,path)
+    }
+
+    return nil
+
+  })
 }
